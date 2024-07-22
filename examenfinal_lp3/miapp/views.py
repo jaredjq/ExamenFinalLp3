425e010816f45dbad9542606cdccfa562b4607a1
Jaredjq <jaredjq@gmail.com>
2024-07-22 15:28:07 -0500

    Jimenez Examen Final - Parte 1 - Site

---
+++ examenfinal_lp3/miapp/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+
+layout = """
+    <h1> Proyecto Web (LP3 - 2024) | Flor Cerdán </h1>
+    <hr/>
+    <ul>
+        <li>
+            <a href="/inde"> Inicio</a>
+        </li>
+        <li>
+            <a href="/saludo"> Mensaje de Saludo</a>
+        </li>
+        <li>
+            <a href="/rango"> Mostrar Números [a,b]</a>
+        </li>
+        <li>
+            <a href="/rango2/10/15"> Mostrar Números [10,15]</a>
+        </li>
+    </ul>
+    <hr/>
+"""
+
+
+def index(request):
+    return render(request + 'index.html')
+
+
+def personas(request):
+    return render(request,'saludo.html')
+
+def rango(request):
+    a = 10
+    b = 20
+    resultado = f"""
+        <h2> Números de [{a},{b}] </h2>
+        Resultado: <br>
+        <ul> 
+    """
+    
+    while a<=b:
+        resultado +=  f"<li> {a} </li>"
+        a+=1
+    resultado += "</ul"
+    return HttpResponse(layout + resultado)
+
+
+def rango2(request,a,b):
+    resultado = f"""
+        <h2> Números de [{a},{b}] </h2>
+        Resultado: <br>
+        <ul> 
+    """
+    
+    while a<=b:
+        resultado +=  f"<li> {a} </li>"
+        a+=1
+    resultado += "</ul"
+    return HttpResponse(layout + resultado)
